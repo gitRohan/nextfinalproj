7f5e913beb9bc7fa221ca966960744e73b9bbceb
Rohan_2703 <101584793+gitRoHaN@users.noreply.github.com>
2025-05-20 19:00:50 +0530

    Create page.tsx

---
+++ src/app/(auth)/sign-in/[[...rest]]/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { SignIn } from "@clerk/nextjs"
+import { useSearchParams } from "next/navigation"
+
+const Page=()=>{
+    const searchParams=useSearchParams()
+    const intent=searchParams.get("intent")
+    return (
+        <div className="w-full flex-1 flex items-center justify-center">
+          <SignIn forceRedirectUrl={intent ? `/dashboard?intent=${intent}` : "/dashboard"} />
+        </div>
+    )
+}
+export default Page
